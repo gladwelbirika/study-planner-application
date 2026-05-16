da1ade220a5a85c9c669c65140538d29e3217836
Gladwel Birika <gladwel.birika@student.moringaschool.com>
2026-05-16 09:02:41 +0300

    setup react frontend routing and login page stucture

---
+++ src/pages/Login.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import API from "../utils/api";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await API.post("/auth/login", {
+        email,
+        password,
+      });
+
+      localStorage.setItem("token", res.data.token);
+
+      alert("Login successful");
+
+      window.location.href = "/dashboard";
+    } catch (error) {
+      alert(error.response?.data?.message || "Login failed");
+    }
+  };
+
+  return (
+    <div style={{ maxWidth: "400px", margin: "auto" }}>
+      <h2>Login</h2>
+
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <br />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
